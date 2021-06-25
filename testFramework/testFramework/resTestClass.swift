9a2503ef5b3817ae4626d8c1e2babf9b5f08502f
namik kaya <nkaya@invio.com>
2021-06-25 20:35:56 +0300

    Test commit 1

---
+++ testFramework/testFramework/resTestClass.swift
@@ -0,0 +1,27 @@
+//
+//  resTestClass.swift
+//  testFramework
+//
+//  Created by namik kaya on 25.06.2021.
+//
+
+import Foundation
+
+protocol ResTestClassDelegate:AnyObject {
+    func rest(cvp: String?)
+}
+
+extension ResTestClassDelegate {
+    func rest(cvp: String?) {}
+}
+
+class resTestClass:NSObject {
+    weak var delegate:ResTestClassDelegate?
+    override init() {
+        super.init()
+    }
+    
+    func question(q: String?) {
+        delegate?.rest(cvp: "\(q ?? "") - el cevap")
+    }
+}
